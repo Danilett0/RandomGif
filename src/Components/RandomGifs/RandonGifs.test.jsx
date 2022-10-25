e5dbd67da5c8591aeb6ffb31259779916d2eba11
jeferson.castro <jeferson.castro@repremundo.com.co>
2022-10-25 14:44:10 -0500

    incluyo test dsos

--- src/Components/RandomGifs/RandonGifs.test.jsx
+++ src/Components/RandomGifs/RandonGifs.test.jsx
@@ -3,7 +3,30 @@ import {render} from '@testing-library/react'
 import RandonGifs from "./RandonGifs"
 
 
-
-test("RandomGif Render", async () => {
+test('renders RandonGifs', async () => {
+    const props = 'gatos'
+    const {findAllByRole} = render(<RandonGifs props={props}></RandonGifs>)
     
-})
+    const apiCatComponents = await findAllByRole("heading")
+  
+    expect(apiCatComponents[0]).toHaveTextContent(props)
+  
+   })
+  
+   test('renders RandonGifs img', async () => {
+    const palabra = 'gatos'
+
+    const {findAllByRole} = render(<RandonGifs props={palabra}></RandonGifs>)
+
+    expect(findAllByRole('img'))
+  
+   })
+  
+   test('renders RandonGifs', async () => {
+    const palabra = 'gatos'
+    
+    const {findAllByRole} = render(<RandonGifs props={palabra}></RandonGifs>)
+  
+    expect(findAllByRole('h1'))
+  
+   })
--- src/Components/RandomWords/RandomWords.test.jsx
+++ src/Components/RandomWords/RandomWords.test.jsx
@@ -3,8 +3,22 @@ import {render} from '@testing-library/react'
 import RandomWords from "./RandomWords"
 
 
+test('renders RandomWords', async () => {
 
-test("RandomGif Render", async () => {
-
-
-})
+    const {findAllByRole} = render(<RandomWords></RandomWords>)
+    
+    const apiCatComponents = await findAllByRole("img")
+  
+    expect(apiCatComponents[0]).toBeInTheDocument()
+  
+   })
+  
+   test('renders RandonGifs', async () => {
+   
+    const {findAllByRole} = render(<RandomWords />)
+    
+    const apiCatComponents = await findAllByRole("heading")
+  
+    expect(apiCatComponents[0]).toHaveClass("mainRandomW")
+  
+   })
