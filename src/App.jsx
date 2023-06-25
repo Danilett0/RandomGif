b61f72011ce4930c3693130c23e45bf1a2608555
Danilett0 <daniilo.97@hotmail.com>
2023-06-25 13:55:01 -0500

    cambio gestor para rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter  as Router, Route, Routes } from "react-router-dom";
 const WelcomePage = lazy(() => import("./Pages/WelcomePage"));
 const MainPage = lazy(() => import("./Pages/MainPage"));
 const NotFoundPage = lazy(() => import("./Pages/NotFoundPage"));
